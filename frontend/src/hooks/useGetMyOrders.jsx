44236c139cc70a4f0b5081b1d3d83a4f64c6f163
princekumar-018 <princessmbscc@gmail.com>
2026-04-02 23:13:52 +0530

    Feature/prince/backend (#3)
    * [Pragati]Handling Structure
    
    * [Pragati Bansal] Folder Structure
    
    * [Pragati] Adding userAuthentication Pages to Auth Branch
    
    * [Pragati Bansal]  add redux store and slices for state management
    
    * [Pragati Bansal]  add redux store and slices for state management
    
    * [Pragati Bansal]  add react hooks for state management
    
    ---------
    
    Co-authored-by: pragati-2024 <bpragati21@gmail.com>

---
+++ frontend/src/hooks/useGetMyOrders.jsx
@@ -0,0 +1,28 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+import { serverUrl } from "../App";
+import { useDispatch, useSelector } from "react-redux";
+import { setMyOrders, setUserData } from "../redux/userSlice";
+
+function useGetMyOrders() {
+  const dispatch = useDispatch();
+  const { userData } = useSelector((state) => state.user);
+  useEffect(() => {
+    if (!userData) {
+      return;
+    }
+    const fetchOrders = async () => {
+      try {
+        const result = await axios.get(`${serverUrl}/api/order/my-orders`, {
+          withCredentials: true,
+        });
+        dispatch(setMyOrders(result.data));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchOrders();
+  }, [userData]);
+}
+
+export default useGetMyOrders;
