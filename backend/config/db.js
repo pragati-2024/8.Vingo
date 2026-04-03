51927f95a6f695f9ff52f7dc387bea0d4738bd14
raginiSahu11 <ragini.sahu@icloud.com>
2026-04-03 08:37:14 +0530

    [Ragini] managing database

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose"
 
 const connectDb=async () => {
     try {
+        
         await mongoose.connect(process.env.MONGODB_URL)
         console.log("db connected")
     } catch (error) {
