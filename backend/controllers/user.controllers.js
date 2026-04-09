5d14bd906a6db90d4584939820568c2999cbdc30
raginiSahu11 <ragini.sahu@icloud.com>
2026-04-09 18:11:32 +0530

    [Ragini] managing controllers

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js"
 
 export const getCurrentUser=async (req,res) => {
+  
     try {
         const userId=req.userId
         if(!userId){
@@ -11,12 +12,14 @@ export const getCurrentUser=async (req,res) => {
                return res.status(400).json({message:"user is not found"})
         }
         return res.status(200).json(user)
-    } catch (error) {
+    } 
+    catch (error) {
         return res.status(500).json({message:`get current user error ${error}`})
     }
 }
 
 export const updateUserLocation=async (req,res) => {
+    
     try {
         const {lat,lon}=req.body
         const user=await User.findByIdAndUpdate(req.userId,{
@@ -25,13 +28,17 @@ export const updateUserLocation=async (req,res) => {
                 coordinates:[lon,lat]
             }
         },{new:true})
-         if(!user){
+        
+        if(!user){
                return res.status(400).json({message:"user is not found"})
         }
         
         return res.status(200).json({message:'location updated'})
-    } catch (error) {
+    } 
+    
+    catch (error) {
            return res.status(500).json({message:`update location user error ${error}`})
     }
+    
 }
 
