18df0653514f632033fc92af28e35948466b458e
raginiSahu11 <ragini.sahu@icloud.com>
2026-04-09 18:12:33 +0530

    Update item.routes.js

--- backend/routes/item.routes.js
+++ backend/routes/item.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 import isAuth from "../middlewares/isAuth.js";
+
 import {
   addItem,
   deleteItem,
@@ -10,7 +11,9 @@ import {
   getItemsByShop,
   rating,
   searchItems,
-} from "../controllers/item.controllers.js";
+}
+  from "../controllers/item.controllers.js";
+
 import { upload } from "../middlewares/multer.js";
 
 const itemRouter = express.Router();
@@ -23,4 +26,5 @@ itemRouter.get("/get-by-city/:city", isAuth, getItemByCity);
 itemRouter.get("/get-by-shop/:shopId", isAuth, getItemsByShop);
 itemRouter.get("/search-items", isAuth, searchItems);
 itemRouter.post("/rating", isAuth, rating);
+
 export default itemRouter;
