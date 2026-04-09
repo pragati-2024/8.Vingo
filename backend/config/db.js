b095451f7124c08643ecc01f1275f897e4f3b2ae
raginiSahu11 <ragini.sahu@icloud.com>
2026-04-09 18:02:32 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,15 @@
 import mongoose from "mongoose"
-
 const connectDb=async () => {
+
     try {
-        
-        await mongoose.connect(process.env.MONGODB_URL)
+           await mongoose.connect(process.env.MONGODB_URL)
         console.log("db connected")
-    } catch (error) {
+    }
+    
+    catch (error) {
         console.log("db error")
     }
+    
 }
 
 export default connectDb
