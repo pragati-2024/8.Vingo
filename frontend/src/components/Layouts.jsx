f8ec09be3326db4991d6877538d3d9424d53c3f4
Prince Kumar <princessmbsccl@gmail.com>
2026-04-09 19:44:47 +0530

    Updated Layouts

--- frontend/src/components/Layouts.jsx
+++ frontend/src/components/Layouts.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Footer from './Footer'
 import Nav from './Nav'
-import AnimatedStarBackground from './AnimatedStarBackground'
+import AnimatedStarBackground from './AnimatedStarBackground.css'
 
 const Layout = ({ children }) => {
   return (
