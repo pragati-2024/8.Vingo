96aecea348935ccbfcd853aeab79aada1523f33f
pragati-2024 <bpragati21@gmail.com>
2026-04-12 16:17:12 +0530

    DOING CHAANGES

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,19 +1,17 @@
-import React from 'react'
-import Footer from './Footer'
-import Nav from './Nav'
-import AnimatedStarBackground from '../components/AnimatedStarBackground';
+import React from "react";
+import Footer from "./Footer";
+import Nav from "./Nav";
+import AnimatedStarBackground from "./AnimatedStarBackGround";
 
 const Layout = ({ children }) => {
   return (
     <div className="min-h-screen flex flex-col relative overflow-x-hidden">
       <AnimatedStarBackground />
       <Nav />
-      <main className="flex-1 relative z-10">
-        {children}
-      </main>
+      <main className="flex-1 relative z-10">{children}</main>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
